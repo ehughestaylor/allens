5f4a27a8d9a178533e83507def5e5c8012f4828e
Elliott Hughes-Taylor <ehughestaylor@Elliotts-MacBook-Pro.local>
2015-05-28 12:40:33 -0400

    update

--- js/script.js
+++ js/script.js
@@ -5,9 +5,29 @@
   //$('.collapsible').collapsible();
 
 $(document).ready(function(){
+  $('.slider').slider({full_width: true});
   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
   $('.modal-trigger').leanModal();
   //start slider and have slider be full width
-  $('.slider').slider({full_width: true});
-      
+  $('#myCarousel').carousel({
+      interval: 4000
+  });
+
+  // handles the carousel thumbnails
+  $('[id^=carousel-selector-]').click( function(){
+    var id_selector = $(this).attr("id");
+    var id = id_selector.substr(id_selector.length -1);
+    id = parseInt(id);
+    $('#myCarousel').carousel(id);
+    $('[id^=carousel-selector-]').removeClass('selected');
+    $(this).addClass('selected');
+  });
+
+  // when the carousel slides, auto update
+  $('#myCarousel').on('slid', function (e) {
+    var id = $('.item.active').data('slide-number');
+    id = parseInt(id);
+    $('[id^=carousel-selector-]').removeClass('selected');
+    $('[id=carousel-selector-'+id+']').addClass('selected');
+  });
 });
